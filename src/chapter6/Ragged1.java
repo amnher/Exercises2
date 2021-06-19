20cf486fc6ca6d8401983af8ccc2f15f0caf63fa
Herman Cruz <amnher@gmail.com>
2021-06-18 17:43:24 -0700

    New Exercises

--- src/chapter6/Ragged1.java
+++ src/chapter6/Ragged1.java
@@ -1,14 +1,32 @@
 package chapter6;
 
+import java.util.Scanner;
+
 public class Ragged1 {
     public static void main(String[] args) {
-        int[][] triangle =
-                {{11},
+        int[][] triangle = new int[4][];
+        triangle[0] = new int[1];
+        triangle[1] = new int[2];
+        triangle[2] = new int[3];
+        triangle[3] = new int[4];
+        /*{{11},
                 {2,3},
                 {4,5,6},
                 {7,8,9,10}
-                };
+                };*/
+        enterNum(triangle);
         max(triangle);
+        printNum(triangle);
+    }
+    static int[][] enterNum(int[][] numIn) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < numIn.length; i++) {
+            for (int j = 0; j < numIn[i].length; j++) {
+                System.out.print("Please enter number: ");
+                numIn[i][j] = sc.nextInt();
+            }
+        }
+        return numIn;
     }
     static void max(int[][] arrayIn) {
         int result = arrayIn[0][0];
@@ -20,5 +38,14 @@ public class Ragged1 {
             }
         }
         System.out.println(result);
+    }
+    static void printNum(int[][] numIn) {
+        for (int i = 0; i < numIn.length; i++) {
+            for (int j = 0; j < numIn[i].length; j++) {
+                System.out.print(numIn[i][j] + " ");
+
+            }
+            System.out.println();
+        }
     }
 }
